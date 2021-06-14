e0214ca2e4a1cc898d1bbb5f71121d8584bfd0fd
LordZee <l0rclz33@gmail.com>
2021-06-14 14:25:14 -0400

    steps 1-5 done

---
+++ forms.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length, Optional
+
+
+class MessageForm(FlaskForm):
+    """Form for adding/editing messages."""
+
+    text = TextAreaField('text', validators=[DataRequired()])
+
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+class EditProfileForm(FlaskForm):
+    """Edit Profile Form"""
+    username = StringField('Username')
+    email = StringField('E-mail', validators=[Email(), Optional()])
+    image_url = StringField('(Optional) Profile Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    bio = StringField('(Optional) Bio')
+    location = StringField('(Optional) Location')
+    password = PasswordField('Input Password', validators=[DataRequired()])
+
+    
